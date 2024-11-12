aecb0effe6d6351fe1bd13b5ff422a1864c6f588
LucasAlberto0 <lucasalberto4321@gmail.com>
2024-11-11 21:06:57 -0300

    add: entry-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckDepotComponent } from './pages/check-depot/check-depot.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EntryPageComponent } from './pages/entry-page/entry-page.component';
+import { ExitPageComponent } from './pages/exit-page/exit-page.component';
 
 const routes: Routes = [{
   path:'',
@@ -10,6 +12,14 @@ const routes: Routes = [{
 {
   path:'check-depot',
   component: CheckDepotComponent
+},
+{
+  path: 'entry',
+  component: EntryPageComponent
+},
+{
+  path: 'exit',
+  component: ExitPageComponent
 }
 ];
 
